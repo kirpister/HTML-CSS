79bf05d28889a0547a5d367d36e6709b7cf32b1b
Alanko Jenni <s2200726@BC-C02J61EAQ6LR-MacBook-Air.local>
2022-09-15 12:28:38 +0300

    first commit

---
+++ week 1/Task 1/Practice/test.js
@@ -0,0 +1,52 @@
+// let text = "Welcome"; 
+// console.log(text.lenght);
+
+
+// CMD + Shift + 7 to comment out al block of code
+
+// let language = "JavaScript";
+// console.log(language[0]);
+
+// const language = "JavaScript";
+// console.log(language.substring(0, 4));
+
+// let firstName = "Julien";
+// let lastName = "Baker";
+// let string = firstName + " " + lastName;
+// console.log(string);
+
+// let firstName ="Julien";
+// firstName = firstName + "Baker";
+// console.log(firstName);
+
+// let firstName = "Julien";
+// firstName += " Baker";
+// console.log(firstName);
+
+// let language = "JavaScript";
+// console.log(`We are now learning ${language}`);
+
+// let number = "45";
+// console.log(number);
+// console.log(number.toString());
+
+// let str ="42";
+// console.log(Number.parseInt(str, 10));
+
+// let a = 11;
+// let b = "21";
+// console.log(a + Number.parseInt(b));
+// console.log(Number.parseInt("123abc", 10));
+// console.log(Number.parseInt("5 meters", 10));
+
+// let name = "Jenni";
+// console.log(name);
+
+// let language = "C++";
+// language = "JavaScript";
+// console.log(language);
+
+const language = "C++";
+console.log(language);
+language = "Python";
+
